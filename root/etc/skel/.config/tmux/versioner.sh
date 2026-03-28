b7407205f1b427bd579f3abf462bff20ca1cb0f4
Alberto Chiaravalli <abchllbox@gmail.com>
2026-03-28 19:45:46 +0100

    feat: tmux versioner
    Opens GitHub, GitLab, Codeberg and SourceHut remotes in Windows's
    default browser based on the session.

---
+++ root/etc/skel/.config/tmux/versioner.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+cd "$(tmux run "echo #{pane_start_path}")" || exit 1
+url=$(git remote get-url origin 2>/dev/null)
+if [ -z "$url" ]; then
+    echo "No git remote found."
+    exit 1
+fi
+
+# Converting SSH to HTTPS
+case "$url" in
+    git@*)
+        url=${url#git@}
+        url="https://${url%%:*}/${url#*:}"
+        ;;
+    ssh://*)
+        url=${url#ssh://}
+        url=${url#git@}
+        host=${url%%/*}
+        path=${url#*/}
+        url="https://$host/$path"
+        ;;
+esac
+
+# Removing trailing .git
+url=${url%.git}
+
+# Extracting the host
+host=${url#https://}
+host=${host%%/*}
+
+# Opening the remote in Windows's default browser
+case "$host" in
+    github.com|gitlab.com|codeberg.org|git.sr.ht)
+        cmd.exe /c start "" "$url"
+        ;;
+    *)
+        echo "Unsupported git host: $host"
+        ;;
+esac
+
